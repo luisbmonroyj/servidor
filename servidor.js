62e3d0c5ba52694859df54b904fe51ec91788280
luisbmonroyj <luisbmonroyj@proton.me>
2025-04-26 21:04:15 -0500

    index.html y funciones para implementarla. prueba exitosa

--- servidor.js
+++ servidor.js
@@ -6,9 +6,75 @@ const fsPromises = require ('fs').promises;
 
 const PORT = process.env.PORT || 4500;
 
+const serveFile =  async (filePath, contentType, response) => {
+    try{
+        const data = await fsPromises.readFile(filePath,'utf8');
+        response.writeHead(200,{'Content-Type':contentType});
+        response.end(data);
+    } 
+    catch (err) {
+        console.error (err);
+        response.statusCode = 500;
+        response.end();
+    }
+}
+
 const server = http.createServer(
-    (req,res) => {
-        console.log (`${req.url}\t${req.method}`);
+    (serverRequest,serverResponse) => {
+        console.log (`${serverRequest.url}\t${serverRequest.method}`);
+        //imprime la direccion del servidor y el metodo usado
+
+        //adquirir la extension del archivo para manipular el Content-Type y manipular correctamente los archivos
+        const extension = path.extname(serverRequest.url);
+        let contentType;
+            switch (extension){
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.jpg':
+                contentType = 'image/jpeg';
+                break;
+            case '.jpeg':
+                contentType = 'image/jpeg';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.txt':
+                contentType = 'text/plain';
+                break;
+            default:
+                contentType = 'text/html';
+        }
+
+        //filtrar la ruta de los archivos segun su tipo con una sentencia ternaria anidada
+        // condicion ? True : False
+        let filePath = 
+            contentType === 'text/html' && serverRequest.url === '/' ?
+                path.join(__dirname,'views','index.html')
+                : contentType === 'text/html' && serverRequest.url.slice(-1) === '/' ?
+                    path.join(__dirname,'views',serverRequest.url,'index.html')
+                    : contentType === 'text/html'?
+                        path.join(__dirname,'views',serverRequest.url)
+                        : path.join(__dirname,serverRequest.url)
+                
+        //Para que la extension .html  no sea necesaria en el navegador
+        if (!extension && serverRequest.url.slice(-1) !== '/') filePath += '.html';
+        
+        //verificar que el archivo existe para cargarlo en el servidor
+        if (fs.existsSync(filePath)){
+            serveFile(filePath, contentType, serverResponse);
+        }
+        else {
+            //301
+            //404
+        }
     }
 );
 
